6218bfe8e9ce6edd48e37f01635198bb412af3b3
Brodrick George <brodrick.george@yahoo.com>
2020-10-18 01:14:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,5 @@ def nyc_pigeon_organizer(data)
  final_results = data.each_with_object({}) do |(key, value), final_array|
    value.each do |inner_key, name|
      name.each do |name|
-       
+       if !final_array 
 end
